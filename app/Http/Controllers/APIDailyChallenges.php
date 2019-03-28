c41a8647ea01b1794253d1284d800df74509d84e
dhairya <dhairyasenjaliya@gmail.com>
2019-03-28 17:10:20 +0530

    Fixes

--- app/Http/Controllers/APIDailyChallenges.php
+++ app/Http/Controllers/APIDailyChallenges.php
@@ -13,18 +13,15 @@ class APIDailyChallenges extends Controller
 {
     public function DailyChallenges()
     {      
-        $cate = Categorie::select('id')->where('isDaily',true)->get() ; 
-        // dd($cate->toArray()); 
-        // $qry = Question::all()->where('categories','=',$cate->toArray()); //->where('categories',$cate->toArray())->get();  
-        $i; 
-        $count = count($cate);
- 
-        for($i = 1; $i <= $count ;$i++)
-        {
-            $qry = Question::all()->where('categories',$i); //->where('categories',$cate->toArray())->get();  
-            $data[$i] = $qry;  
-        } 
-        return response()->json($data);
+        $cate = Categorie::select('id')->where('isDaily',true)->get();          
+        $qry = Question::select('id','question','answer')->wherein('categories', $cate)->get(); //->where('categories',$cate->toArray())->get();  
+
+        return response()->json($qry);
+
+        //  $cate = Categorie::select('id')->where('isDaily',true)->get() ;
+        //  $qry = Question::select('id','question','answer','image')->where('categories',$cate->toArray())->get();  
+
+        // return response()->json($qry);
         
         // $cate = Categorie::select('id')->where('isDaily',true)->get() ;
         // $qry = Question::orderByRaw("RAND() * 99999")->select('question','answer','image')->where('categories',$cate->toArray())->get();  
